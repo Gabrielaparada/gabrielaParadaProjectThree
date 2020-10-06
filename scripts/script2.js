ee30f74ad87ad445723de016e6875243e9379d32
Gabriela Parada <gabrielaparada@Gabrielas-MacBook-Pro.local>
2020-10-06 18:52:02 -0400

    update

--- scripts/script2.js
+++ scripts/script2.js
@@ -143,7 +143,7 @@ $(document).ready(function () {
          type="text" name="input" id="${input}" placeholder="${input}" required=""><label class="visuallyHidden" for="${input}">${input}</label>`)
       })
       $('.fillWords').append(`<button class="buttonSection3 fontFamilyText" type="submit">Next</button>`)
-      
+
       //grab values from user 
       $('.fillWords').on('submit', function(e){
          e.preventDefault();
@@ -160,7 +160,6 @@ $(document).ready(function () {
          </button>`)
          $('.section4 h3').html((findStory[0].title))
          $('.section4 p').html(findStory[0].completeStory(...results))
-         
       })
       });
    })
